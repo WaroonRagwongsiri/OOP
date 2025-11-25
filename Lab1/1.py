19593873cec05d36f9dd0f58b049cca76e867a68
WaroonRagwongsiri <waroonwork@gmail.com>
2025-11-25 17:02:32 +0700

    1

--- Lab1/1.py
+++ Lab1/1.py
@@ -1,6 +1,10 @@
 def	main():
-	a = float(input("Input: "))
-	print("Output:", a + a*11 + a*111 + a*1111)
+	a = input("Input : ")
+	if (a.isdigit() == False or len(a) != 1):
+		print("Output : Invalid Input")
+		return
+	a = float(a)
+	print("Output :", int(a + a*11 + a*111 + a*1111))
 
 if __name__ == "__main__":
 	main()
