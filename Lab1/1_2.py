6e9aef55075b1903bc5b364c33b8ce5f79243163
WaroonRagwongsiri <waroonwork@gmail.com>
2025-11-25 21:13:56 +0700

    1_2 laibao

--- Lab1/1_2.py
+++ Lab1/1_2.py
@@ -14,14 +14,17 @@ def	main() -> None:
 		print("Invalid Input")
 		return
 	num = int(userin)
-	if (num < 1):
+	if (num < 2):
 		print("Invalid Input")
 		return
 	maxpalin = 0
-	for i in range(1 * (10**(num - 1)), 1 * (10**num)):
-		for j in range(1 * (10**(num - 1)), 1 * (10**num)):
-			if (isPalindrome(str(i * j)) and i * j > maxpalin):
-				maxpalin = i * j
+	for i in range(1 * (10**(num)) - 1, (1 * (10**(num - 1) - 1)), -1):
+		for j in range(1 * (10**(num)) - 1, (1 * (10**(num - 1) - 1)), -1):
+			if (i * j < maxpalin):
+				break
+			if (isPalindrome(str(i * j))):
+				if (i * j > maxpalin):
+					maxpalin = i * j
 				break
 	print(maxpalin)
 
